e7036b9ea280a5747a06356d3b3eac49343ed313
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-15 11:05:34 -0700

    Pass remaining tests for root reducer.
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,6 @@ import { combineReducers } from 'redux';
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   masterTicketList: ticketListReducer
-});
+}); // key is the state slice, value represents the reducer 
 
 export default rootReducer;
